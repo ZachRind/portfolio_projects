d14898f6f8e4c84d0d03b9b1c8e7806508913cfe
Zach Rind <zachrind@gmail.com>
2021-03-06 16:23:53 -0700

    fixing grammar

--- silly_title_generator.js
+++ silly_title_generator.js
@@ -8,13 +8,13 @@ TEMPLATES TO USE
 */
 
 
-let philosophers = ['Plato', 'Aristotle', 'Seneca', 'Cicero', 'Boethius', 'Descartes', 'Montaigne', 'Hobbes', 'Voltaire', 'Kant', 'Hume', 'de Tocqueville', 'Marx', 'Nietzsche', 'Freud', 'HUsserl', 'Russel', 'Wittgenstein', 'Heidegger', 'Gramsci', 'Gadamer', 'Sartre', 'Adorno', 'Marcuse', 'Simone de Beauvoir', 'Aquinas', 'Camus', 'Foucault', 'Chomsky', 'Derrida', 'Žižek'];
+let philosophers = ['Plato', 'Aristotle', 'Seneca', 'Cicero', 'Boethius', 'Descartes', 'Montaigne', 'Hobbes', 'Voltaire', 'Kant', 'Hume', 'de Tocqueville', 'Marx', 'Nietzsche', 'Freud', 'Husserl', 'Russel', 'Wittgenstein', 'Heidegger', 'Gramsci', 'Gadamer', 'Sartre', 'Adorno', 'Marcuse', 'Simone de Beauvoir', 'Aquinas', 'Camus', 'Foucault', 'Chomsky', 'Derrida', 'Žižek'];
 
 let philTopics = ['Semiotics', 'Structuralism', 'Phenomenology', 'Rationalism', 'Utilitarianism', 'Hermeneutics', 'Idealism', 'Existentialism', 'Absurdism', 'Objectivism', 'Marxism', 'Skepticism', 'Relativism', 'Cosmology', 'Cynicism', 'Stoicism', 'Political Theory', 'Empiricism', 'Platonism', 'Mysticism', 'Pedagogy', 'Epistemology', 'Metaphysics', 'Humanism', 'Deontology', 'Transcendentalism', 'Hegemony'];
 
 let popCulRef = ['Wiping Again on the Same Raid Boss', 'Getting Broken Up With Over Text', 'Using Too Many Emojis', 'Taking Selfies', 'Thinking Pineapple Belongs on Pizza', 'Basing Your Personality on Drinking Coffee', 'Taking Buzzfeed Quizzes', 'Feeling Guilty Any Time You Spend Not Working', `Daydreaming How Much You'd Help Your Friends if You Were Rich`, 'Taking Your Dog to the Vet While Ignoring Your Own Health Problems', 'Obtaining the Master Sword', 'Choosing Your Starter Pokemon', 'Getting Killed by an Eight-Year-Old in Fortnite', 'Defeating Bowser for the First Time', 'Punching Trees in Minecraft', 'Patiently Waiting While the Customer in Front of You in Line Attempts to Use Expired Coupons', 'Saying Something Awkward and Thinking About It All Day', 'Holding the Door Open for Someone Far Away and Forcing Them to Run Over', 'Saying Goodbye to Someone Then Walking in the Same Direction', 'Flunking a Job Interview, Getting Up, and Walking Directly into the Coat Closet'];
 
-let philPhenom = ['Dialectical Materialism', 'Post-Colonial Theory', 'Hermeneutic Bodies of Knowledge', 'Semiotic Structures', 'Theories of Technology', 'The Evolution of Modern Metaphysics', 'Contemporary Tractatus', 'Pluralism in Ethics', 'Perception and Intentionality', 'Agency and Free Will', 'Epistemologies of Modernism', 'The Mind-Body Probelm'];
+let philPhenom = ['Dialectical Materialism', 'Post-Colonial Theory', 'Hermeneutic Bodies of Knowledge', 'Semiotic Structures', 'Theories of Technology', 'The Evolution of Modern Metaphysics', 'Contemporary Tractatus', 'Pluralism in Ethics', 'Perception and Intentionality', 'Agency and Free Will', 'Epistemologies of Modernism', 'The Mind-Body Problem'];
 
 let verbTitle = ['Rethinking Tradition', 'Deconstructing Modernity', 'Recentering Reason', 'Obsolete', 'Adamant', 'Boorish or Elegant?', 'No Longer Noxious', 'Reading Between the Lines', 'Questioning Relativism'];
 
