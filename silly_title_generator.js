c7885f0f24a3ff47a00b08e4c27967be314c6b9e
Zach Rind <zachrind@gmail.com>
2021-03-06 13:01:22 -0700

    project outline

---
+++ silly_title_generator.js
@@ -0,0 +1,17 @@
+/*
+
+[Philosopher] on [philisophical topic] and [pop culture reference]
+Understanding [philosophical phenomenon] through [philosopher] and [pop cul ref]
+[Interesting adjective or verb phrase]: The [jargony phrase] of [pop cul ref] 
+How [notable moment in gaming] illustrates [philosopher]'s concept of [philisophical idea]
+[pop cul figure], [philosopher], and [silly activity]: An/A [Noun (introduction, review, brief history, etc.)]
+
+*/
+
+/* 
+
+create arrays to act as word banks
+
+
+
+*/
